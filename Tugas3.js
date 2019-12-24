db561f2cff4034de21f78b07520b6703e65ff256
Alghifari Muhammad Dzar <alghifari.m.d@gmail.com>
2019-12-24 17:16:32 +0300

    Tugas 3 JS

---
+++ Tugas3.js
@@ -0,0 +1,17 @@
+function cekdata(x) {
+var x;
+hasil=x*x;
+
+// cara 1
+console.log('hasil pangkat', x, 'adalah');
+return hasil;
+
+// cara 2
+// return console.log('hasil pangkat', x, 'adalah', hasil);
+}
+
+// cara 1
+console.log(cekdata(100));
+
+// cara 2
+// cekdata(100)
